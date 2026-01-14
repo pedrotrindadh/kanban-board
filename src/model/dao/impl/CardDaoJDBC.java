962f0282c411bac0768d363d8a293ebfaa5db049
Pedro Trindade <prindade@outlook.com>
2026-01-14 12:40:00 -0300

    CardDaoJDBC

---
+++ src/model/dao/impl/CardDaoJDBC.java
@@ -0,0 +1,46 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.CardDao;
+import model.entities.Card;
+
+public class CardDaoJDBC implements CardDao {
+
+	@Override
+	public void create(Card card) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Card card) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(Card card) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Card findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Card> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
