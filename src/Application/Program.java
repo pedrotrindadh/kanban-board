71469808e850217c267f2f722d8a18cc6e9d7f71
Pedro Trindade <prindade@outlook.com>
2026-01-22 11:26:23 -0300

    Adding methods Program

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,46 +1,81 @@
 package Application;
 
-import static model.entities.enums.CardStatus.*;
-
-import java.util.Date;
-import java.util.List;
-
-import model.dao.CardDao;
-import model.dao.DaoFactory;
-import model.entities.Card;
+import java.util.Scanner;
 
 public class Program {
+	
+	public static final Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
-		CardDao cardDao = DaoFactory.createdCardDao();
-		
-		System.out.println("========FIRST TEST: findAll CardDao========");
-		List<Card> list = cardDao.findAll();
-		for (Card obj:list) {
-			System.out.println(obj);
+		while(true) {
+			System.out.println("Select one of the following options:");
+			System.out.println("1 - Show the board");
+			System.out.println("2 - Filter a card");
+			System.out.println("3 - Create a card");
+			System.out.println("4 - Edit the title and topic of card");
+			System.out.println("5 - Move the status of card");
+			System.out.println("6 - Remove a card");
+			System.out.println("7 - Exit");
+			
+			int option = sc.nextInt();
+			
+			switch(option) {
+				case 1:
+					showBoard();
+					break;
+				case 2:
+					filterCard();
+					break;
+				case 3:
+					createCard();
+					break;
+				case 4:
+					editTitleTopic();
+					break;
+				case 5:
+					moveStatus();
+					break;
+				case 6:
+					removeCard();
+					break;
+				case 7:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Option invalid");
+					break;
+			}
 		}
+	}
+
+	private static void showBoard() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("========SECOND TEST: findById CardDao========");
-		Card id = cardDao.findById(1);
-		System.out.println(id);
+	}
+
+	private static void filterCard() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("========THIRD TEST: create CardDao========");
-		Card newCard = new Card(null, "BugFix", "Fix feature of NewBank", new Date(), TO_DO);
-		cardDao.create(newCard);
-		System.out.println("Created a new card, Id: " + newCard.getId());
+	}
+
+	private static void createCard() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("========FOURTH TEST: update CardDao========");
-		Card updateCard = new Card(7, "Meet", "Meet with all squad", null, null);
-		cardDao.update(updateCard);
-		System.out.println("Card Updated!");
+	}
+
+	private static void editTitleTopic() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("========FIFTH TEST: move CardDao========");
-		Card moveCard = new Card(3, null, null, null, APPROVED);
-		cardDao.move(moveCard);;
-		System.out.println("Card moved!");
+	}
+
+	private static void moveStatus() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("========SIXTH TEST: delete CardDao========");
-		cardDao.delete(3);
-		System.out.println("Card deleted!");
+	}
+
+	private static void removeCard() {
+		// TODO Auto-generated method stub
+		
 	}
 }
